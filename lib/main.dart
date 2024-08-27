ace85ea3f5f0c320a2b024aaf98809cc6ebb52b1
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-08-27 21:58:13 +0530

    chore: Update main.dart to use MultiProvider for managing app theme and item data

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:theme_explorer/providers/items_provider.dart';
+import 'package:theme_explorer/providers/theme_provider.dart';
 import 'package:theme_explorer/screens/home_screen.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(create: (context) => ThemeProvider()),
+      ChangeNotifierProvider(create: (context) => ItemsProvider()),
+    ],
+    child: const MainApp(),
+  ));
 }
 
 class MainApp extends StatelessWidget {
@@ -12,9 +19,14 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => ItemsProvider(),
-      child: const MaterialApp(home: HomeScreen()),
-    );
+    ThemeProvider themeProvider = Provider.of<ThemeProvider>(context);
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        themeMode: themeProvider.themeMode,
+        theme: ThemeData(
+            scaffoldBackgroundColor: themeProvider.scaffoldBGColor,
+            colorScheme: const ColorScheme.light()),
+        darkTheme: ThemeData(colorScheme: const ColorScheme.dark()),
+        home: const HomeScreen());
   }
 }
